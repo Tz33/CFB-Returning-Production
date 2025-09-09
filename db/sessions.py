c651a24b1b1f4617bec307b56132f11f90fce268
Tyler Zuluaga <tyler.zuluaga33@gmail.com>
2025-09-09 17:11:21 -0400

    Add DM session utility

---
+++ db/sessions.py
@@ -0,0 +1,14 @@
+import os
+from dotenv import load_dotenv
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+load_dotenv()
+
+DATABASE_URL = os.geten("DATABASE_URL")
+if not DATABASE_URL:
+    raise RuntimeError("DATABASE_URL not set. Put it in .env")
+
+engine = create_engine(DATABASE_URL, future=True, echo=False)
+SessionLoacal = sessionmaker(bind=engine, autoflush=False, autocommit=False, future=True)
+
